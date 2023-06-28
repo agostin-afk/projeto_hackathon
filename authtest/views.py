cedc9b4ebb5b9e2106d68fe6ce0f7765797e08a7
Agostinho Ferreira <agostinho008.af45@gmail.com>
2023-06-28 11:39:16 -0300

    adição do método do form e css

--- authtest/views.py
+++ authtest/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 
 def cadastro(request):
-    return render(request, 'cadastro.html')
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+    else: 
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        return 
 
 
 def login(request):
