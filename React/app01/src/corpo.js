1cf7891d95ec368560525894e89ce376327a77a2
Agostinho Ferreira <agostinho008.af45@gmail.com>
2023-08-08 20:01:12 -0300

    Update corpo.js

--- React/app01/src/corpo.js
+++ React/app01/src/corpo.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Relogio from  './Relogio'
 export default function Corpo(props){
     return(
-        <section class='Corpo'>
+        <section className='Corpo'>
             <h1 id='corpoh1'>Esse é o corpo </h1>
             <p>Seu nome é: {props.nome}</p>
             <p>Você possui {props.peso}kg</p>
