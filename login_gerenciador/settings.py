e9a1d27b45d0cffd4958dd8c842e70bd611e6fcd
Agostinho Ferreira <agostinho008.af45@gmail.com>
2023-08-08 20:30:58 -0300

    implementação do postgres

--- login_gerenciador/settings.py
+++ login_gerenciador/settings.py
@@ -67,8 +67,11 @@ WSGI_APPLICATION = 'login_gerenciador.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '*****',
+        'USER': '*****',
+        'PASSWORD': '*****',
+        'HOST': '*****',
     }
 }
 
