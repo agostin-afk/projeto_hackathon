34181a8a46bad767de1849844992f9ff26832a41
Agostinho Ferreira <agostinho008.af45@gmail.com>
2023-08-14 12:32:05 -0300

    sistema de pagina
    o sistema de navegar entre as paginas está funcionando, talvez tenha mudado um pouco mas nada preocupante

--- projeto_hackathon/settings.py
+++ projeto_hackathon/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -29,7 +30,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-    'projeto_hackathon',
+    'app',
 ]
 
 SITE_ID = 1
@@ -49,7 +50,7 @@ ROOT_URLCONF = 'projeto_hackathon.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
