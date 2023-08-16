a8a43ef24255f0e71efc7af7c8fe5a200e73d36f
Agostinho Ferreira <agostinho008.af45@gmail.com>
2023-08-15 21:09:39 -0300

    aproveitando o código antigo
    para o fluxo de paginas nas views

--- app/views.py
+++ app/views.py
@@ -1,7 +1,48 @@
+from django.http import HttpResponse
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as login_django
+from django.contrib.auth.decorators import login_required
 def cadastro(request):
-    return render(request, 'cadastro.html')
-
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+    else: 
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        
+        user = User.objects.filter(username = username).first()
+        if user: 
+            return HttpResponse('já existe um usuário com esse nome, tente novamente')
+        
+        user = User.objects.create_user(username, email, senha)
+        
+        return HttpResponse(username)
 def login(request):
-    return render(request, 'login.html')
+    if request.method == "GET":
+        return render(request, 'login.html')
+    else: 
+        username = request.POST.get("username")
+        senha = request.POST.get("senha")
+        user = authenticate(username=username,password=senha)
+        
+        if user: 
+
+            login_django(request, user)
+
+            return render(request, "produtos.html")
+        else: 
+            return HttpResponse("Email ou senha inválidos")
+'''      
+sem o login_required:
+def produtos(request):
+    if request.user.is_authenticated:
+        return HttpResponse('pagina dos produtos')
+    else: 
+        return HttpResponse("você precisa estar logado!!!")
+'''
+@login_required(login_url="/auth/login/")
+def produtos(request):
+    if request.method == "GET":
+        return render(request, 'produtos.html')
